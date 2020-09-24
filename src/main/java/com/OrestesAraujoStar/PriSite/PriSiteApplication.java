a4ece372e52e982203375783026e3f4f211ba4d6
OrestesAraujo-Star <orestesaraujo@gmail.com>
2020-09-24 17:06:46 -0300

    Projeto criado

---
+++ src/main/java/com/OrestesAraujoStar/PriSite/PriSiteApplication.java
@@ -0,0 +1,13 @@
+package com.OrestesAraujoStar.PriSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OrestesAraujoStar/PriSite/PriSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrestesAraujoStar.PriSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
